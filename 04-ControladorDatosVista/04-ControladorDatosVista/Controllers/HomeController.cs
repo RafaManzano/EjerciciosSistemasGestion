04ee0f7a2f40c76836b30d2de9e25c201022dff1
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-10 14:30:46 +0200

    Subiendo

---
+++ 04-ControladorDatosVista/04-ControladorDatosVista/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_ControladorDatosVista.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(DateTime.Now.Hour < 12)
+            {
+                ViewData["saludo"] = "Buenos Dias";
+            }
+            else if(DateTime.Now.Hour < 20)
+            {
+                ViewData["saludo"] = "Buenas tardes";
+            }
+            else
+            {
+                ViewData["saludo"] = "Buenas noches";
+            }
+            return View();
+        }
+    }
+}
---
+++ 04-ControladorDatosVista/04-ControladorDatosVista/Models/clsPersona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ControladorDatosVista.Models
+{
+    public class clsPersona
+    {
+        //Fernando usa para la propiedad privada asi _nombre, pero yo usare minusculas y para los metodos
+        //Mayusculas
+        private string nombre;
+        //private string primerApellido;
+        //private string segundoApellido;
+        //private DateTime fechaNacimiento;
+
+        public string Nombre {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
