6fcdfa6d4fd016f521cbfc54511ab68c28d0ff32
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-10-23 19:48:57 +0200

    Subiendo

---
+++ 08-ExamenSorpresa/08-ExamenSorpresa/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using _08_ExamenSorpresa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _08_ExamenSorpresa.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<clsDepartamento> departamentos = new List<clsDepartamento>();
+            departamentos.Add(new clsDepartamento(1, "INFORMATICA"));
+            departamentos.Add(new clsDepartamento(2, "FINANZAS"));
+            departamentos.Add(new clsDepartamento(3, "CONTABILIDAD"));
+            departamentos.Add(new clsDepartamento(4, "VENTAS"));
+
+            clsPersonaDepartamento personaConDepartamento = new clsPersonaDepartamento();
+            personaConDepartamento.Persona = new clsPersona("Rafael", "Manzano", "Medina", DateTime.Now);
+            personaConDepartamento.Departamentos = departamentos;
+            return View(personaConDepartamento);
+        }
+
+        [HttpPost]
+        public ActionResult Index(clsPersona persona)
+        {
+            return View("PersonaModificada", persona);
+        }
+    }
+}
---
+++ 08-ExamenSorpresa/08-ExamenSorpresa/Models/clsPersonaDepartamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _08_ExamenSorpresa.Models
+{
+    public class clsPersonaDepartamento
+    {
+        public clsPersona Persona { get; set; }
+        public List<clsDepartamento> Departamentos{ get; set; }
+    }
+}
