aeff51790c4279c8b9ca9cd03b96d9b21a791bde
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-24 13:24:53 +0200

    Terminado

---
+++ 09-ExamenSorpresa-Ejercicio2/09-ExamenSorpresa-Ejercicio2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _09_ExamenSorpresa_Ejercicio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(bool? esMiPrimeraVez)
+        {
+            if(esMiPrimeraVez == true){
+                ViewBag.texto = "No es la primera vez que entras";
+            }
+            else
+            {
+                ViewBag.texto = "Es es la primera vez que entras";
+            }
+                
+            
+            return View();
+        }
+    }
+}
---
+++ 09-ExamenSorpresa-Ejercicio2/09-ExamenSorpresa-Ejercicio2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_09_ExamenSorpresa_Ejercicio2.MvcApplication" Language="C#" %>
