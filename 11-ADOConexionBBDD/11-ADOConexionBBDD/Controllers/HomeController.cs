6ff0f8f8c28e13225a3478c930a5728ed2bb2daa
RafaManzano <rafamanzanomedina@gmail.com>
2019-11-05 14:29:19 +0100

    Subiendo

---
+++ 11-ADOConexionBBDD/11-ADOConexionBBDD/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace _11_ADOConexionBBDD.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection conn = new SqlConnection();
+
+       
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Index")]
+        public ActionResult Garbancito()
+        {
+            try
+            {
+                conn.ConnectionString = "server=rafamanzano.database.windows.net;database=Personas;uid=rmanzano;pwd=MiTesoro.;";
+                conn.Open();
+                ViewBag.Error = conn.State;
+            }
+            catch
+            {
+                ViewBag.Error = conn.State;
+            }
+
+            return View("Index", ViewBag.Error);
+        }
+    }
+}
---
+++ 11-ADOConexionBBDD/11-ADOConexionBBDD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_11_ADOConexionBBDD.MvcApplication" Language="C#" %>
