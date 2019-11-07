5b553c7a795f519fec98669b9ce2408760ea4a58
RafaManzano <rafamanzanomedina@gmail.com>
2019-11-07 14:28:29 +0100

    Empezando
    Todavia nada, solo estamos creando las conexiones y poco mas

---
+++ 12-CRUD-Personas/12-CRUD-Personas-Entities/clsDepartamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_CRUD_Personas_Entities
+{
+    class clsDepartamento
+    {
+        public String nombre { get; set; }
+        public int id { get; set; }
+
+        public clsDepartamento(String nombre, int id)
+        {
+            this.nombre = nombre;
+            this.id = id;
+        }
+    }
+}
