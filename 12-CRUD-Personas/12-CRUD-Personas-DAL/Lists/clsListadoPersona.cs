841d2fa9d7bc39945f917fd425c1af3d182e02e9
RafaManzano <rafamanzanomedina@gmail.com>
2019-11-13 14:31:07 +0100

    Empezando

---
+++ 12-CRUD-Personas/12-CRUD-Personas-DAL/Lists/clsListadoPersona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_CRUD_Personas_DAL.Lists
+{
+    class clsListadoPersona
+    {
+        /*
+        public List<clsPersona> listadoPersonas()
+        {
+            byte[] bytes = new byte[8];
+            List<clsPersona> listado = new List<clsPersona>;
+            listado.Add(new clsPersona("Rafael", "Manzano Medina", DateTime.Now, "600600600", 1, 1, bytes));
+            listado.Add(new clsPersona("Pepe", "Rodriguez Medina", DateTime.Now, "600600600", 1, 1, bytes));
+            listado.Add(new clsPersona("Jose", "Perez Lopez", DateTime.Now, "600600600", 1, 1, bytes));
+            listado.Add(new clsPersona("Maria", "Gonzalez Sanchez", DateTime.Now, "600600600", 1, 1, bytes));
+        }
+        */
+    }
+}
---
+++ 12-CRUD-Personas/12-CRUD-Personas-UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_12_CRUD_Personas_UI.Global" Language="C#" %>
