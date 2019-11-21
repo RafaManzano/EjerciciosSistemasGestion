1fb1e148dbfe9d0d8a6a7753d6968614ee159faa
RafaManzano <rafamanzanomedina@gmail.com>
2019-11-21 14:24:49 +0100

    Api

---
+++ 12-CRUD-Personas/12-CRUD-Personas-UI/Controllers/API/PersonaController.cs
@@ -0,0 +1,45 @@
+﻿using _12_CRUD_Personas_BL.Lists;
+using _12_CRUD_Personas_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _12_CRUD_Personas_UI.Controllers.API
+{
+    public class PersonaController : ApiController
+    {
+        private clsListadoPersonasBL capaBL = new clsListadoPersonasBL();
+        // GET: api/Persona
+        public IEnumerable<clsPersona> Get()
+        {
+            return capaBL.listadoPersonas();
+        }
+
+        // GET: api/Persona/5
+        public clsPersona Get(int id)
+        {
+            return capaBL.personaPorID(id);
+        }
+
+        // POST: api/Persona
+        public void Post([FromBody]clsPersona value)
+        {
+            capaBL.crearPersona(value);
+        }
+
+        // PUT: api/Persona/5
+        public void Put(int id, [FromBody]clsPersona value)
+        {
+            capaBL.actualizarPersona(value);
+        }
+
+        // DELETE: api/Persona/5
+        public void Delete(int id)
+        {
+            capaBL.borrarPersona(id);
+        }
+    }
+}
