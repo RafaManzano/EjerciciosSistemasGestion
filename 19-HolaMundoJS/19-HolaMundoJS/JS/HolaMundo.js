dbd60159bf01e371fa70e9a5e81c44770d504708
RafaManzano <rafamanzanomedina@gmail.com>
2020-01-09 14:25:18 +0100

    HolaMundo

---
+++ 19-HolaMundoJS/19-HolaMundoJS/JS/HolaMundo.js
@@ -0,0 +1,10 @@
+﻿window.onload = inicializa;
+
+function inicializa() {
+    //Anhadir evento click al boton Pulsar y que cambie el texto
+    document.getElementById("btnPulsar").addEventListener("click", cambiarTexto, false);
+}
+
+function cambiarTexto() {
+    document.getElementById("divTexto").innerHTML = "HolaMundo";
+}
