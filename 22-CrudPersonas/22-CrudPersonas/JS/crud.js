e8c218bb5c4c22653847fdfc4b94985747f84f32
RafaManzano <rafamanzanomedina@gmail.com>
2020-01-20 14:06:37 +0100

    Mas modificando

--- 22-CrudPersonas/22-CrudPersonas/JS/crud.js
+++ 22-CrudPersonas/22-CrudPersonas/JS/crud.js
@@ -1,9 +1,15 @@
 ﻿window.onload = inicializa;
+var editar;
+var anhadir;
+var eliminar;
 
 function inicializa() {
     document.getElementById("anhadir").addEventListener("click", introducirPersona);
     document.getElementById("editar").addEventListener("click", clickarEditar);
     cargarPersonasConDepartamentos();
+    editar = document.getElementById("editEmployeeModal");
+    anhadir = document.getElementById("addEmployeeModal");
+    eliminar = document.getElementById("deleteEmployeeModal");
     //document.getElementById("btnPedirApellido").addEventListener("click", pedirApellido);
     //document.getElementById("btnBorrar").addEventListener("click", borrarPersona);
 }
@@ -57,12 +63,13 @@ function cargarPersonas(arrayDepartamentos) {
                     data +="<td id='nombre'>" + arrayPersonas[i].nombre + "</td>";
                     data +="<td id='apellidos'>" + arrayPersonas[i].apellidos + "</td>";
                     data +="<td id='fecha'>" + arrayPersonas[i].fechaNacimiento + "</td>";
-                    data +="<td id='direccion'>" + arrayPersonas[i].direccion + "</td>";
+                    data += "<td id='direccion'>" + arrayPersonas[i].direccion + "</td>";
+                    data += "<td id='telefono'>" + arrayPersonas[i].telefono + "</td>";
                     data += "<td id='foto'>" + arrayPersonas[i].foto + "</td>";
                     data += "<td id='nombreDpto'>" + nombreDpto + "</td>";
                     data += '<td> ' +
                         //'<button id="editar">Editar</button>'
-                        '<button id="editar" class="material-icons" data-toggle="tooltip" title="Editar" onclick="clickarEditar()" >'+ arrayPersonas[i].idPersona +'</button>' +
+                        '<i class="material-icons" data-toggle="tooltip" title="Editar" id="editar" onclick="clickarEditar(' + arrayPersonas[i].idPersona + ')">&#xE254;</i>' +
                         '<a id="borrar" href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete" >&#xE872;</i></a>' +
                         '</td>';
                     data += "</tr>"
@@ -121,65 +128,78 @@ function introducirPersona() {
 }
 
 
-function clickarEditar() {
-    var x = document.getElementById("editar");
-    //var persona = new Object();
-    persona = buscarPersonaPorID(this.id);
-    document.getElementById("nombreE").innerHTML = persona.nombre;
-    document.getElementById("apellidosE").innerHTML = persona.apellidos;
-    document.getElementById("fechaE").innerHTML = persona.fechaNacimiento;
-    document.getElementById("direccionE").innerHTML = persona.direccion;
-    document.getElementById("telefonoE").innerHTML = persona.telefono;
-    document.getElementById("fotoE").innerHTML = persona.foto;
-    document.getElementById("#addEmployeeModal").modal();
-   
-    //var editar = new XMLHttpRequest();
-    //editar.open("PUT", "https://crudtoflamaapi.azurewebsites.net/api/Persona", false);
-    //editar.setRequestHeader('Content-type', 'application/json');
+function clickarEditar(id) {
+    var guardar = document.getElementById("guardarE");
+    var persona = new Object();
+    persona = buscarPersonaPorID(id);
+    //document.getElementById("idE").value = persona.id;
+    document.getElementById("nombreE").value = persona.nombre;
+    document.getElementById("apellidosE").value = persona.apellidos;
+    document.getElementById("fechaE").value = persona.fechaNacimiento;
+    document.getElementById("direccionE").value = persona.direccion;
+    document.getElementById("telefonoE").value = persona.telefono;
+    document.getElementById("fotoE").value = persona.foto;
+    document.getElementById("nombreDptoE").value = persona.nombreDpto;
 
+    guardar.onclick = function () {
+        var nuevaPersona = new Object();
+        nuevaPersona.id = id;
+        nuevaPersona.nombre = document.getElementById("nombreE").value;
+        nuevaPersona.apellidos = document.getElementById("apellidosE").value;
+        nuevaPersona.fechaNacimiento = document.getElementById("fechaE").value;
+        nuevaPersona.direccion = document.getElementById("direccionE").value;
+        nuevaPersona.telefono = document.getElementById("telefonoE").value;
+        nuevaPersona.foto = document.getElementById("fotoE").value;
+        nuevaPersona.idDepartamento = 1;
+
+        var editarLlamada = new XMLHttpRequest();
+        editarLlamada.open("PUT", "https://crudtoflamaapi.azurewebsites.net/api/Persona" + id, false);
+        editarLlamada.setRequestHeader('Content-type', 'application/json');
+
+        var json = JSON.stringify(nuevaPersona);
+
+        //Definicion estados
+        editarLlamada.onreadystatechange = function () {
+
+            if (editarLlamada.readyState < 4) {
+
+
+            }
+            else
+                if (editarLlamada.readyState == 4 && editarLlamada.status == 200) {
+
+                    alert("Persona actualizada con exito");
+                    editar.style.display = "none";
+
+                }
+        };
+
+        editarLlamada.send(json);
+
+    }
 
     /*
-     * for (var c = 0; c < arrayDepartamentos.length; c++) {
-        if (arrayDepartamentos[c].nombre == document.getElementById("nombreDptoA").innerText)
-            var nombreDpto = arrayDepartamentos[c].nombre;
-        var idDpto = arrayDepartamentos[c].id;
-    }
+     * Esto es para la foto
+    var img = document.createElement('img');
+    img.src = "data:image/jpg;base64," + listadoPersonas[i][this.col[j]];
+    img.height = 30;
+    img.width = 30;
+    tabCell.appendChild(img);
     */
 
-    //var persona = new Object();
-    //persona.nombre = document.getElementById("nombreA").value;
-    //persona.apellidos = document.getElementById("apellidosA").value;
-    //persona.fechaNacimiento = document.getElementById("fechaA").value;
-    //persona.direccion = document.getElementById("direccionA").value;
-    //persona.telefono = document.getElementById("telefonoA").value;
-    //persona.foto = document.getElementById("fotoA").value;
-    //persona.idDepartamento = 1;
-    //persona.nombreDpto = nombreDpto;
-    //persona.idDepartamento = idDpto;
-
-    //var json = JSON.stringify(persona);
-
-    // Definicion estados
-    //insertar.onreadystatechange = function () {
-
-        //if (insertar.readyState < 4) {
+    editar.style.display = "block";
+   
+}
 
 
-        //}
-        //else
-            //if (insertar.readyState == 4 && insertar.status == 204) {
-
-                //alert("Persona insertada con exito");
-            //}
-    //};
-
-    //insertar.send(json);
+function cancelarEditar() {
+    editar.style.display = "none";
 }
 
 
 function buscarPersonaPorID(id) {
     var miLlamada = new XMLHttpRequest();
-    miLlamada.open("GET", "https://vuestraapipersonas/api/Personas/" + id);
+    miLlamada.open("GET", "https://crudtoflamaapi.azurewebsites.net/api/Persona/" + id, false);
     var persona = new Object();
 
     //Definicion estados
@@ -187,15 +207,14 @@ function buscarPersonaPorID(id) {
 
         if (miLlamada.readyState < 4) {
 
-            //aquí se puede poner una imagen de un reloj o un texto “Cargando”
 
         }
         else
             if (miLlamada.readyState == 4 && miLlamada.status == 200) {
 
 
-               persona  = JSON.parse(miLlamada.responseText);
-                
+                persona = JSON.parse(miLlamada.responseText);
+               
 
             }
 
@@ -204,3 +223,5 @@ function buscarPersonaPorID(id) {
     miLlamada.send();
     return persona;
 }
+
+
