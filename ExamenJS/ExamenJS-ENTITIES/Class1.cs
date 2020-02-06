7b756de14f0d989fbe62ce3cb9a6ef35827dd57a
RafaManzano <rafamanzanomedina@gmail.com>
2020-02-06 08:30:56 +0100

    Examen

---
+++ ExamenJS/ExamenJS-ENTITIES/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenJS_ENTITIES
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExamenJS/ExamenJS-UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamenJS_UI.Global" Language="C#" %>
